27e56853ed06c4a86a70a84c16b77395a0fdd29c
Brijesh R <56794851+brijesh81@users.noreply.github.com>
2021-01-01 16:50:00 +0530

    country picker updated

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { NativeSelect, FormControl} from '@material-ui/core';
+import './CountryPicker.css';
+import {countries} from '../../api';
 
 const CountryPicker = () => {
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+    useEffect(() => {
+        const fetchedCountries = async () => {
+            setFetchedCountries(await countries);
+        }
+
+        fetchedCountries();
+    }, [setFetchedCountries]);
+
     return (
-        <h1>CountryPicker</h1>
+        <FormControl className="formControl">
+            <NativeSelect>
+                <option value="global">Global</option>
+
+            </NativeSelect>
+        </FormControl>
     )
 }
 
